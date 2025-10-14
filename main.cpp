ddbfa09be33a16f24fc03f4ff6a32ca0caad4d14
svgabriiel <svgabriiel@gmail.com>
2025-10-14 12:08:13 -0300

    oi
    teste

--- main.cpp
+++ main.cpp
@@ -1,73 +1,57 @@
-
-
-/*******************************************************************************************
-*
-*   raylib [core] example - basic window
-*
-*   Example complexity rating: [â˜…â˜†â˜†â˜†] 1/4
-*
-*   Welcome to raylib!
-*
-*   To test examples, just press F6 and execute 'raylib_compile_execute' script
-*   Note that compiled executable is placed in the same folder as .c file
-*
-*   To test the examples on Web, press F6 and execute 'raylib_compile_execute_web' script
-*   Web version of the program is generated in the same folder as .c file
-*
-*   You can find all basic examples on C:\raylib\raylib\examples folder or
-*   raylib official webpage: www.raylib.com
-*
-*   Enjoy using raylib. :)
-*
-*   Example originally created with raylib 1.0, last time updated with raylib 1.0
-*
-*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
-*   BSD-like license that allows static linking with closed source software
-*
-*   Copyright (c) 2013-2025 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
-
 #include "raylib.h"
+#include <stdio.h>
 
 //------------------------------------------------------------------------------------
-// Program main entry point
+/// Função principal do programa
 //------------------------------------------------------------------------------------
 int main(void)
 {
-    // Initialization
+    // Inicialização
     //--------------------------------------------------------------------------------------
     const int screenWidth = 800;
     const int screenHeight = 450;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    InitWindow(screenWidth, screenHeight, "RESTA UM");
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    SetTargetFPS(60);               /// Define o jogo para rodar a 60 quadros por segundo
     //--------------------------------------------------------------------------------------
 
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+    // Loop principal do jogo
+    while (!WindowShouldClose())    /// Detecta se o botão de fechar foi clicado ou se ESC foi pressionado
     {
-        // Update
+        // Atualização
         //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
+        // TODO: Atualize suas variáveis aqui
         //----------------------------------------------------------------------------------
 
-        // Draw
+
+
+
+        // Desenho
         //----------------------------------------------------------------------------------
         BeginDrawing();
 
-            ClearBackground(RAYWHITE);
+            ClearBackground(WHITE);
 
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+            DrawText("Gabriel", 50, 190, 50, BLACK);
+
+            const int x=0,y=0;
+
+                if(x == const int 300) x=0;
+                if(y == const int 300) y=0;
+
+                DrawText("\nMANICRAFT", 190 + x, 250 + y, 50, BLUE);
+
+                x=+5;
+                y=+5;
 
         EndDrawing();
         //----------------------------------------------------------------------------------
     }
 
-    // De-Initialization
+    // Desinicialização
     //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
+    CloseWindow();        // Fecha a janela e o contexto OpenGL
     //--------------------------------------------------------------------------------------
 
     return 0;
