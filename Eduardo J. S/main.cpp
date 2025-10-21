9bfd4372cbabbb915de98adb0d1c7527e8948bd8
edusts-SI <edusts82@gmail.com>
2025-10-21 18:13:57 -0300

    Mudança de tema preto ou branco
    Adicionada  a função booleana locale_Muda e a função booleana trocarTema.

--- Eduardo J. S/main.cpp
+++ Eduardo J. S/main.cpp
@@ -4,48 +4,68 @@
 
 int main(void)
 {
-    setlocale(LC_ALL, "Portuguese"); // configura a linguagem para português
-    
+    setlocale(LC_ALL, "Portuguese");
     
     // Initialization
-    //--------------------------------------------------------------------------------------   
-
-    InitWindow(screenWidth, screenHeight, "TELA 60FPS");
-
-    SetTargetFPS(60);             // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
-
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
-    {
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-
-            
-
-        //----------------------------------------------------------------------------------
-
-        // Draw
-        
-                
-        //----------------------------------------------------------------------------------
-        BeginDrawing();
-
-        ///------------------
-        /*/ PLAY GAME ---> */renderiza_Jogo();
-        ///-------------------
+    InitWindow(screenWidth, screenHeight, "RESTA UM - 60FPS");
+    SetTargetFPS(60);
     
-        ClearBackground(BLACK);// COR DE FUNDO
+    stateJogo jogo = INICIO;
+    int i_inicial = 0, j_inicial = 0;
+        
+    // Main game loop
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(corFundo);
+
+        switch (jogo) {
+            case INICIO:
+                Emblema();
+                if(locale_Button()) {
+                    if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+                        inicializa_tabuleiro(tabuleiro);
+                        startTime = GetTime();  // TIMER INICIA
+                        jogo = PLAY;
+                    }
+                }
+                break;
+
+            case PLAY:
+            Atualiza_Imformação();
+            localize_Part(tabuleiro, &i_inicial, &j_inicial);
+            desenha_tabuleiro(tabuleiro, i_inicial, j_inicial, false);
+            Jogada();
+            Titulo();
+            
+            if(locale_Reset()) {
+                if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+                    startTime = GetTime();
+                    jogo = RESET;
+                }
+            }
+
+            if(locale_Muda()){
+                if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+                    trocarTema();
+                    std::cout << "Botão TEMA clicado!" << std::endl;
+                }
+                // Highlight do botão TEMA
+                for(int i = 0; i < 4; i++) {
+                    DrawRectangleLines(30-i, 500-i, 100+i, 45+i, GREEN);
+                }
+            }
+            break;
+                
+            case RESET:
+                inicializa_tabuleiro(tabuleiro);
+                jogo = PLAY;
+                break;
+        }
 
         EndDrawing();
-        //----------------------------------------------------------------------------------
     }
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
-
+    CloseWindow();
     return 0;
 }
