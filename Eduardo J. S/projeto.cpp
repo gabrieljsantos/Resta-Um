4caa86f467c539f66f72c678681e46dc618b6aa1
edusts-SI <edusts82@gmail.com>
2025-10-20 22:57:58 -0300

    versão branca com time
    Refactor game logic and button handling for improved functionality.

--- Eduardo J. S/projeto.cpp
+++ Eduardo J. S/projeto.cpp
@@ -4,7 +4,7 @@
 //----------------------
 // Variáveis globais
 //----------------------
-bool pare = true;
+
 int pode=0;
 Part tabuleiro[TAM][TAM];
 int Ic = 0, Jc= 0;
@@ -14,60 +14,54 @@ int inicioX = screenWidth/3.5, inicioY = screenHeight/4; // TENTA PEGAR A MELHOR
                                                          // PARA CENTRALIZAR O TABULEIRO
 Vector2 posição = {(float)inicioX,(float)inicioY};
 botao menu[dois] = {
-    { true, {700.0f, 530.0f} },
-    { true, {800.0f, 530.0f} }
+    { true, {740.0f, 530.0f} },
+    { true, {830.0f, 530.0f} }
 };
 //----------------------------------------------------------------------------
-// IMPLEMENTAÇÕES DE FUNÇÕES E PROCEDIMENTOS NO GERAL
+// IMPLEMENTAÇÕES DE FUNÇÕES NO GERAL
 //----------------------------------------------------------------------------
-// PROCEDIMENTO Botoes ---> Para Criar Botões
+// Função Botoes ---> Para Criar Botões
 //----------------------------------------------------------------------------
 void Botoes(botao(&menu)[dois]){
     for(int i = 0; i < 2; i++){
         if(menu[i].state){
-            DrawCircle(menu[i].local.x, menu[i].local.y, (float)raio, (i==0)? BLUE : GREEN);
-            DrawText("RESET",670, 554, 20, PURPLE); 
+            DrawCircle(menu[i].local.x, menu[i].local.y, (float)raio, (i==0)? BLUE : BLACK);
+            DrawText("RESET",711, 554, 20, BLACK);
+            DrawText("RESET",711, 556, 20, BLACK);   
         }
     }
 }
 //----------------------------------------------------------------------------
-// PROCEDIMENTO inicializar_tabuleiro ---> Para Gerar Tudo Do Jogo
+// Função inicializar_tabuleiro ---> Para Gerar Tudo Do Jogo
 //----------------------------------------------------------------------------
 void inicializa_tabuleiro(Part (&tabuleiro)[TAM][TAM]) {
-
+    //int inicioX = screenWidth/3.5, inicioY = screenHeight/4; 
     for (int i = 0; i < TAM; i++) {
         for (int j = 0; j < TAM; j++) {
             tabuleiro[i][j].pos ={ (float)inicioX + i * (diametro + espaco),
                                    (float)inicioY + j * (diametro + espaco) };
             if ((i < 2 || i > 4) && (j < 2 || j > 4))
-                tabuleiro[i][j].state = N_EXIST; // NAO EXISTE
+                tabuleiro[i][j].state = 0; // NAO EXISTE
             else
-                tabuleiro[i][j].state = EXIST; // PEÇA VIVA
+                tabuleiro[i][j].state = 2; // PEÇA VIVA
         }
     }
-    tabuleiro[3][3].state = VAZIO; // AREA LIVRE
+    tabuleiro[3][3].state = 1; // AREA LIVRE
 }
 //----------------------------------------------------------------------------
-// PROCEDIMENTO desenha_tabuleiro ---> Para Mostar Graficamente as Peças
+// Função desenha_tabuleiro ---> Para Mostar Graficamente as Peças
 //----------------------------------------------------------------------------
 void desenha_tabuleiro(Part (&tabuleiro)[TAM][TAM], int i_atual, int j_atual, bool red) {
     for (int i = 0; i < TAM; i++) {
         for (int j = 0; j < TAM; j++) {
             if (tabuleiro[i][j].state == 0) continue;
-
-            Color cor = (tabuleiro[i][j].state == 1) ? LIGHTGRAY : PURPLE; // COR DAS PEÇAS 
+            Color cor = (tabuleiro[i][j].state == 1) ? LIGHTGRAY : BEIGE; // COR DAS PEÇASE VAZIO
             DrawCircleV(tabuleiro[i][j].pos, raio, cor);
-
             for(int o = 0; o < 3; o++)
                 DrawCircleLinesV(tabuleiro[i][j].pos, raio + o, BLACK);
             if (i == i_atual && j == j_atual) {
-                
-       /*s aux*/ int aux = 0;//PARA FUNÇAO (VALIDA PART)
-   /*MOVIMENTO*/ bool Mov = valida_Part(tabuleiro, i,j, aux);
-                Color Cor = (Mov)? GREEN:RED;
-
                 for (int k = 0; k < 4; k++)
-                    DrawCircleLinesV(tabuleiro[i][j].pos, raio + k, Cor);
+                    DrawCircleLinesV(tabuleiro[i][j].pos, raio + k, (valida_Part(tabuleiro,i,j, &k))? GREEN: RED);
             }
         }
     }
@@ -92,10 +86,10 @@ bool localiza_BOTAO(botao (&menu)[dois], int* li){
     Vector2 mouse = GetMousePosition(); // POSIÇÃO DO MOUSE
     double menor = diametro;
     for(int i = 0; i < dois; i++){
-        double a = menu[i].local.x - mouse.x; // DELTA DE X
-        double b = menu[i].local.y - mouse.y;// DELTA  DE Y
-        double c = sqrt( pow(a,2) + pow(b,2)); // HIPOTENUSA 
-        if (c < menor){ // SE HIPOTENUSA FOR MENOR QUE O DDIAMETRO
+        double a = menu[i].local.x - mouse.x;
+        double b = menu[i].local.y - mouse.y;
+        double c = sqrt( pow(a,2) + pow(b,2));
+        if (c < menor){
             *li = i; 
             menor =c;
         }
@@ -104,7 +98,6 @@ bool localiza_BOTAO(botao (&menu)[dois], int* li){
 }
 //----------------------------------------------------------------------------
 // Função localize_Part ---> Para Localizar Peças Existentes
-// DENTRO DO RAIO DA PEÇA
 //----------------------------------------------------------------------------
 bool localize_Part(Part (&tabuleiro)[TAM][TAM], int* ii, int* jj, int state) {
     int e;
@@ -131,87 +124,66 @@ bool localize_Part(Part (&tabuleiro)[TAM][TAM], int* ii, int* jj, int state) {
     return menor <= (double)raio;
 }
 //----------------------------------------------------------------------------
-// FUNÇÃO SUPER IMPORTANTE
 // Função Valida_Part ---> Para Validar possiveis jogadas
 //----------------------------------------------------------------------------
-bool valida_Part(Part(&tabuleiro)[TAM][TAM], int Io, int Jo, int (&S)){
+bool valida_Part(Part(&tabuleiro)[TAM][TAM], int It, int Jt, int* S){
 
-    int Id, Jd, Im,Jm; /*-- Io,Jo origem, Id,Jd Destinho, Im,Jm Meio --*/
-
-       S = 0; // nao tem movimento valido
-       // S retorna o valor por referencia
-        if(tabuleiro[Io][Jo].state == N_EXIST) //parte que esta fora (em branco)
+    int Io, Jo, Id, Jd, Im,Jm; /*-- Io,Jo origem, Id,Jd Destinho, Im,Jm Meio --*/
+        if(tabuleiro[It][Jt].state ==0){
             return false;
-        if(tabuleiro[Io][Jo].state != EXIST) // nao é uma peça valida
-            return false;
-
-        //---------------------------------------------------------------------
-
-        //agora tenta os movimentos valido para achar a peça sem estar travada
-        //---------------------------------------------------------------------
-        for (int i = 0; i < 4; i++) {
-            int Di = Movimentos[i][0]; //|DELTA ESTA RECEBENDO MOVIMENTOS DO TIPO (I-2, I+2, J-2, J+2)
-            int Dj = Movimentos[i][1]; //| QUE SAO MOVIMENTOS(ESQUERDA, DIREITA, CIMA, BAIXO)
-            Id = Io + Di; // i destinho = pos. atual + Delta i
-            Jd = Jo + Dj; // j destinho = pos. atual + Delta j
-
-            Im = Io + Di/2; // i meio = i origem + Delta /2 
-            Jm = Jo + Dj/2; // j meio = i origem + Delta /2 
-            
-            //------------------------------------------------------------
-            if (Id < 0 || Id >= TAM || Jd < 0 || Jd >= TAM) continue;
-            if (Im < 0 || Im >= TAM || Jm < 0 || Jm >= TAM) continue;
-            //verifica se a jogada da peça pode sair do tabuleiro
-            //------------------------------------------------------------
-
-            if ((tabuleiro[Id][Jd].state == VAZIO )&&(tabuleiro[Im][Jm].state != VAZIO)){
-                S = 1; // um movimento valido
-                return true; //Existe alguma jogada valida
-            }
         }
-        // se chegou ate aqui
-        S = 0; 
+
+        if((tabuleiro[It][Jt].state !=1 )){//diferente de peça vazia = (1,0)
+            
+            for (int i = 0; i < 4; i++) {
+                int Di = Movimentos[i][0]; //|DELTA ESTA RECEBENDO MOVIMENTOS DO TIPO I-2, I+2, J-2, J+2
+                int Dj = Movimentos[i][1]; //| QUE SAO MOVIMENTOS(ESQUERDA, DIREITA, CIMA, BAIXO)
+
+                Id = It + Di; // i destinho = pos. atual + Delta i
+                Jd = Jt + Dj; // j destinho = pos. atual + Delta j
+
+                Im = It + Di/2; // i meio = i origem + Delta /2 
+                Jm = Jt + Dj/2; // j meio = i origem + Delta /2 
+
+                if ((tabuleiro[Id][Jd].state == 1 )&&(tabuleiro[Im][Jm].state != 1)){
+                    *S;
+                    return true; //Existe alguma jogada valida
+                }
+
+            }
+
+        }
+        
+    
 
     return false;
 }
 //----------------------------------------------------------------------------
-// PROCEDIMENTO Jogada ---> Para Fazer o Movimento no Jogo
-//CAPITURA OS CLIQUES E VERIFICA SE PODE JOGAR
+// Função Jogada ---> Para Fazer o Movimento no Jogo
 //----------------------------------------------------------------------------
 void Jogada(void){
-   if(!pare) DrawText("MOVIMENTO INVALIDO",250,90,30,RED);
-   
+
     if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
     
-        if (clique_atual == CLIQUE_PRIMEIRO  ) {
-            pare = false;
+        if (clique_atual == CLIQUE_PRIMEIRO) {
             if (localize_Part(tabuleiro, &Ic, &Jc, 2)) {
                 std::cout << "Primeiro clique na posição X: " << tabuleiro[Ic][Jc].pos.x << ", Y: " << tabuleiro[Ic][Jc].pos.y << std::endl;
             }
-         } else {
+        } else {
             int Ifim = 0, jfim = 0;
             if (localize_Part(tabuleiro, &Ifim, &jfim, 1)) {
-               std::cout << "Segundo clique na posição X: " << tabuleiro[Ifim][jfim].pos.x << ", Y: " << tabuleiro[Ifim][jfim].pos.y << std::endl;                
-               int movimento = calcule_movimento(Ic, Jc, Ifim, jfim);
-                
+                std::cout << "Segundo clique na posição X: " << tabuleiro[Ifim][jfim].pos.x << ", Y: " << tabuleiro[Ifim][jfim].pos.y << std::endl;
+                int movimento = calcule_movimento(Ic, Jc, Ifim, jfim);
                 if (movimento == MOVIMENTO_INVALIDO) {
-                   pare = true;
-                    DrawText("MOVIMENTO INVALIDO",250,90,30,RED);
-                
-
-                    
-                  //  Sleep(1000);
+                    std::cout << "Movimento inválido " << "I: " << Ic << " J: " << Jc
+                     << " para I2: " << Ifim
+                      << " J2: " << jfim << std::endl;
                 } else {
                     int i_alvo = (Ic + Ifim) / 2; //i meio = i origem + Delta i /2
                     int j_alvo = (Jc + jfim) / 2;//i meio = i origem + Delta j /2
-
-                    if (tabuleiro[i_alvo][j_alvo].state == EXIST) {
-                         std::cout << "Deletando em i = " << i_alvo << "; j = " << j_alvo << std::endl;
-                        //-----------------------------------//
-                        tabuleiro[i_alvo][j_alvo].state = VAZIO; //
-                        tabuleiro[Ifim][jfim].state = EXIST;     //  atualiza o estado das peças movidas
-                        tabuleiro[Ic][Jc].state = VAZIO;         //
-                        //-----------------------------------//
+                    if (tabuleiro[i_alvo][j_alvo].state == 2) {
+                        std::cout << "Deletando em i = " << i_alvo << "; j = " << j_alvo << std::endl;
+                        tabuleiro[i_alvo][j_alvo].state = 1;tabuleiro[Ifim][jfim].state = 2;tabuleiro[Ic][Jc].state = 1;
                     } 
                 }
                 if (movimento != MOVIMENTO_NENHUM) {Ic = 0; Ifim = 0; Jc= 0; jfim = 0;
@@ -222,69 +194,57 @@ void Jogada(void){
     }
 }
 
-//----------------------------------------------------------------------------
-// Função RESTA UM ---> Para verificar quantidade de peças sem movimento valido
-//----------------------------------------------------------------------------
-bool jogada_Valida(Part(&tabuleiro)[TAM][TAM], int (&Resta)) {
-    int Nvalido = 0, Valido = 0;
+// FALTA TERMINAR
+bool Resta_Um(Part(&tabuleiro)[TAM][TAM], int *R) {
+    int Nvalido = 0, Valido = 0, S = 0,aux;
 
     for (int i = 0; i < TAM; i++) {
         for (int j = 0; j < TAM; j++) {
-            int S = 0;
-
-            if ((tabuleiro)[i][j].state == N_EXIST) continue;
-            //NAO PODE TESTAR PARTE EM BRAMCO DA ERRO!!!!
-
-            valida_Part(tabuleiro, i, j, S);
-                
-            if (S == 1) {
-                 Valido++;
-            }
-            else{
-            // PEÇAS SEM MOVIMENTO VALIDO
-                if(tabuleiro[i][j].state==EXIST)
+            if (valida_Part(tabuleiro, i, j, &S)) {
+                Valido++;
+                if (S == 1) {
                     Nvalido++;
+                }
             }
-            
         }
     }
-    Resta = Nvalido;
+
+    aux = Nvalido;
+    std::cout << aux<< "\n";
+
 
     if (Valido > 0) {
-        //std::cout << "RESTA JOGO\n";
+        std::cout << "RESTA JOGO\n";
         return RESTA_JOGADA; // ainda há jogadas possíveis
     } else {
-        //std::cout << "NAO RESTA\n";
+        std::cout << "NAO RESTA\n";
         return NAO_RESTA_JOGADA; // jogo terminou
     }
-}//---------------------------------------------------------------------------
+}
 
 //----------------------------------------------------------------------------
-// PROCEDIMENTO Titulo ---> Para Mostrar EMBLEMA DO JOGO
+// Função Titulo ---> Para Mostrar Titulo
 //----------------------------------------------------------------------------
 void Emblema(void){
     
-    DrawText( "Resta Um",inicioX-80 , inicioY, 100, PURPLE);
+    DrawText( "Resta Um",inicioX-80 , inicioY, 100, BLACK);
 }
 //----------------------------------------------------------------------------
-// PROCEDIMENTO Titulo ---> Para Mostrar Titulo
+// Função Titulo ---> Para Mostrar Titulo
 //----------------------------------------------------------------------------
 void Titulo(void){
     int R;
-    DrawText("----- RESTA UM -----", 252, 50, 30,PURPLE);//
-    DrawText("----- RESTA UM -----", 252, 50, 30,PURPLE);///------| Em cima
+    DrawText("----- RESTA UM -----", 252, 50, 30,BLACK);//
+    DrawText("----- RESTA UM -----", 252, 50, 30,BLACK);///------| Em cima
+   // DrawText("----- RESTA UM -----", 256, 53, 30,BLACK);//------| do tabuleiro
+    //DrawText("----- RESTA UM -----", 256, 53, 30,BLACK);//
+    if(Resta_Um(tabuleiro, &R))
+        std :: cout << R;
+    else
+    DrawText("GAME OVER",900/4,100,50,RED);
 
-    if(!jogada_Valida(tabuleiro, R)){ // se nao ouver jogada valida game over ou vitoria
-
-        if (R == 1) DrawText("Vitória", 100, 50, 30, BLUE);
-        else{
-        DrawText("GAME OVER",900/2 -100,600/2,30,RED);
-        DrawText(TextFormat("Restaram: %d", R), 100, 150, 30, PURPLE);
-        }
-    }    
-    
+    int ii;
 }
-
 //----------------------------------------------------------------------------
 // Função DisplayTimer ---> Para Exibir o Tempo Decorrido
 //----------------------------------------------------------------------------
@@ -298,7 +258,7 @@ void DisplayTimer(float timeElapsed){
 
 
 //----------------------------------------------------------------------------
-// PROCEDIMENTO Atualiza_Informacao ---> Para Verificar OS Cliques no MOUSE
+// Função Capitura_Informacao ---> Para Verificar OS Cliques no MOUSE
 //----------------------------------------------------------------------------
 void Atualiza_Imformação(){
     
@@ -306,7 +266,7 @@ void Atualiza_Imformação(){
     int i_atual = 0, j_atual = 0;
     localize_Part(tabuleiro, &i_atual, &j_atual);
     if (clique_atual == CLIQUE_PRIMEIRO) {
-        desenha_tabuleiro(tabuleiro, i_atual, j_atual, tabuleiro[i_atual][j_atual].state != EXIST);
+        desenha_tabuleiro(tabuleiro, i_atual, j_atual, tabuleiro[i_atual][j_atual].state != 2);
     } else {
         int movimento = calcule_movimento(Ic, Jc
             , i_atual, j_atual);
@@ -314,7 +274,7 @@ void Atualiza_Imformação(){
     }
 }
 //----------------------------------------------------------------------------
-// PROCEDIMENTO renderiza Jogo ---> Para Renderizar e Fucionar o JOGO
+// Função Valida_Part ---> Para Validar possiveis jogadas
 //----------------------------------------------------------------------------
 void renderiza_Jogo(){
     static float startTime = 0.0f;
@@ -322,34 +282,19 @@ void renderiza_Jogo(){
     float timeElapsed = GetTime() - startTime; // Calcula o tempo decorrido
     Jogada();
     int ii;
-
-    //--------------------------------
-    // --> Informações sobre os cliques
-     Atualiza_Imformação();
-    //------------------------------------
-    
-    //--------------------------------
-    // --> Play GAME
-     Jogada();
-    //------------------------------------
-    
-
-     if(localiza_BOTAO(menu,&ii)) {
-         if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)&&ii==0) {
-             inicializa_tabuleiro(tabuleiro);
-             pode++;
-             startTime = GetTime(); // Reseta o timer ao reiniciar o jogo
-            
-            
-            } 
-     } 
-     //DisplayTimer(timeElapsed); // Exibe o timer
-    //---------------------------------
-    // --> DESENHOS
+    if(localiza_BOTAO(menu,&ii)) {
+        if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)&&ii==0) {
+            inicializa_tabuleiro(tabuleiro);
+            pode++;
+            startTime = GetTime(); // Reseta o timer ao reiniciar o jogo
+        }
+        if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)&&ii==1) std::cout << "CLICK "<< ii << "\n";
+    } 
     Botoes(menu);
     (pode==0)? Emblema() : Titulo();
-    if(pode >= 1)return DisplayTimer(timeElapsed); 
-    
-    
-
+    if(localiza_BOTAO(menu,&ii)){
+        for(int i =0;i<4;i++)
+            DrawCircleLinesV(menu[ii].local,(float)raio +i , (ii==0)? GREEN:RED);
+    }
+    DisplayTimer(timeElapsed); // Exibe o timer
 }
