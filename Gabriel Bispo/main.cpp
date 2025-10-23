b42081030bd946d2810f63f5a2a9ebf7b881cf0b
Gabriel Bispo <svgabriiel@gmail.com>
2025-10-23 13:12:43 -0300

    FINAL 23/10

--- Gabriel Bispo/main.cpp
+++ Gabriel Bispo/main.cpp
@@ -9,14 +9,14 @@ using namespace std;
 int main(void)
 {
     setlocale(LC_ALL, "Portuguese");
-    cout << "\033[32m"; // Muda a cor do texto para verde
+    cout << "\033[32m"<<std::endl; // Muda a cor do terminal para verde
 
     // Initialization
     InitWindow(screenWidth, screenHeight, "RESTA UM - 60FPS");
     SetTargetFPS(144);
 
     stateJogo jogo = INICIO;
-    int i_inicial = 0, j_inicial = 0;
+    int i_inicial = 0, j_inicial = 0,r;
 
     // Main game loop
     while (!WindowShouldClose())
@@ -41,6 +41,7 @@ int main(void)
                     desenha_tabuleiro(tabuleiro, i_inicial, j_inicial, clique_atual);
                     Jogada();
                     Titulo();
+                    if(!jogada_Valida(tabuleiro,r)) jogo = FIMDEJOGO;
 
                     if(locale_Reset()) 
                         if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
@@ -58,11 +59,30 @@ int main(void)
                 break;
 
                 case RESET:
+                    desenha_tabuleiro(tabuleiro,i_inicial,j_inicial,clique_atual);
                     Titulo();
                     startTime = GetTime();
                     inicializa_tabuleiro(tabuleiro);
                         jogo = PLAY;
-                    break;
+
+                break;
+
+                case FIMDEJOGO:
+                    desenha_tabuleiro(tabuleiro,i_inicial,j_inicial,clique_atual);
+                    Titulo();
+                    if(locale_Reset()) 
+                        if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+                        jogo = RESET;
+                        }
+                    
+
+                    if(locale_Muda()){
+                        if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+                            trocarTema();
+                        }
+                
+                    }
+                break;
         }
 
         EndDrawing();
