5f752130e8845be587f061abad5c0b35257eebbb
Gabriel Bispo <svgabriiel@gmail.com>
2025-10-22 22:51:41 -0300

    FINAL
    Updated game initialization and main loop logic.

--- Gabriel Bispo/main.cpp
+++ Gabriel Bispo/main.cpp
@@ -1,94 +1,74 @@
+// iNCLUSÃO DE BIBLIOTECAS
+
 #include "raylib.h"
+#include <locale.h>
 #include "projeto.h"
+#include <iostream>
+using namespace std;
 
 int main(void)
 {
+    setlocale(LC_ALL, "Portuguese");
+    cout << "\033[32m"; // Muda a cor do texto para verde
+
     // Initialization
-    //--------------------------------------------------------------------------------------   
+    InitWindow(screenWidth, screenHeight, "RESTA UM - 60FPS");
+    SetTargetFPS(144);
 
-    InitWindow(screenWidth, screenHeight, "TELA 60FPS");
-    int i_inicial=0, j_inicial=0;
-
-   
-    SetTargetFPS(60);             // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
     stateJogo jogo = INICIO;
-        
+    int i_inicial = 0, j_inicial = 0;
+
     // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+    while (!WindowShouldClose())
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-
-            
-
-        //----------------------------------------------------------------------------------
-
-        // Draw 
-        
-                
-        //----------------------------------------------------------------------------------
-        
         BeginDrawing();
+        ClearBackground(corFundo);
 
-        switch (jogo){
+        switch (jogo) {
+            case INICIO:
+                    Emblema();
+                    if(locale_Button()) {
+                        if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+                            inicializa_tabuleiro(tabuleiro);
+                            startTime = GetTime(); 
+                            jogo = PLAY;
+                        }
+                    }
+                break;
 
-        case INICIO:
-            Emblema();
-            if(locale_Button()){
-                if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
+            case PLAY:
+                    localize_Part(tabuleiro, &i_inicial, &j_inicial);
+                    desenha_tabuleiro(tabuleiro, i_inicial, j_inicial, clique_atual);
+                    Jogada();
+                    Titulo();
+
+                    if(locale_Reset()) 
+                        if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+                        jogo = RESET;
+                        }
+                    
+
+                    if(locale_Muda()){
+                        if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+                            trocarTema();
+                        }
+                
+                    }
+            
+                break;
+
+                case RESET:
+                    Titulo();
+                    startTime = GetTime();
                     inicializa_tabuleiro(tabuleiro);
-                    jogo=PLAY;
-                }
-            }
-            break;
-        case PLAY:
-            //DrawRectangle(inicioX-40,inicioY-40,440,440,BROWN);
-            Atualiza_Imformação();
-            desenha_tabuleiro(tabuleiro,i_inicial,j_inicial,-1);
-            Jogada();
-            Titulo();
-            if(locale_Button()){
-                if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
-                    jogo = RESET;
-                }
-            }
-            break;
-        case RESET:
-            inicializa_tabuleiro(tabuleiro);
-            jogo=PLAY;
-            break;
-        
-        // case TEMA:
-        //     /* code */
-        //     break;
-        
-        // case FIMDEJOGO:
-        //     /* code */
-        //     break;
-        
-        default:
-
-            break;
+                        jogo = PLAY;
+                    break;
         }
 
-
-
-        ///------------------
-        /*/ PLAY GAME ---> *///renderiza_Jogo();
-        ///-------------------
-    
-        ClearBackground(BLACK);// COR DE FUNDO
-
         EndDrawing();
-        //----------------------------------------------------------------------------------
     }
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
-
+    CloseWindow();
     return 0;
 }
+
