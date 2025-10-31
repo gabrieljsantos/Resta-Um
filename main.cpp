1dc1988bb8afdfc6b5a9f6eba969cc9c6cb2940a
Gabriel Bispo <svgabriiel@gmail.com>
2025-10-30 23:53:47 -0300

    30/10

--- main.cpp
+++ main.cpp
@@ -2,23 +2,23 @@
 #include <locale.h>
 #include "projeto.h"
 
-using namespace std;
-
 int main(void)
 {
     setlocale(LC_ALL, "Portuguese");
-    cout << "\033[32m"<<std::endl; // Muda a cor do terminal para verde
+   
 
     // INICIALIZAÇÃO
     InitWindow(screenWidth, screenHeight, "RESTA UM - 60FPS");
-    SetTargetFPS(fps);
+    
 
     stateJogo jogo = main_Menu;
-    int i_inicial = 0, j_inicial = 0, aux;
+    int i_inicial = 0, j_inicial = 0, aux;//aux para auxiliar (Poder usar )
 
     // Main game loop
+
     while (!WindowShouldClose())
-    { 
+    {   
+        SetTargetFPS(fps);
         BeginDrawing();
         ClearBackground(corFundo);
 
@@ -37,19 +37,20 @@ int main(void)
             case STARTGAME:
                     localizePart(tabuleiro, &i_inicial, &j_inicial);
                     desenhaTabuleiro(tabuleiro, i_inicial, j_inicial, clique_atual);
-                    Jogada();
                     Titulo();
-                     
-                    if(locateButton(restartButton)) // Botão restart
+                    Jogada();
+                    if(locateButton(resetButton)) // Botão restart
                         if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
                         jogo = RESETGAME;
                         }
                     if(locateButton(themeButton)){ // Botão tema
                         if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
                             trocarTema();
+                    
                         }
-                    if(!jogadaValida(tabuleiro,aux)) jogo = ENDGAME;// se NÃO TIVER JOGADA VALIDA
                     }
+                    if(!jogadaValida(tabuleiro,aux)) jogo = ENDGAME;// se NÃO TIVER JOGADA VALIDA
+                
             
                 break;
 
@@ -65,7 +66,7 @@ int main(void)
                 case ENDGAME:
                     desenhaTabuleiro(tabuleiro, i_inicial, j_inicial, clique_atual);
                     Titulo();
-                    if(locateButton(restartButton)) 
+                    if(locateButton(resetButton)) 
                         if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
                         jogo = RESETGAME;
                         }
